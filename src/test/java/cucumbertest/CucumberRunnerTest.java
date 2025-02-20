8c61ea71d60ca1562822582fae0287a9220bcbc1
Aleh Sinkel <aleh.sinkel@sqilsoft.by>
2025-02-20 21:56:18 +0300

    clarified import constants for cucumber runner class

--- src/test/java/cucumbertest/CucumberRunnerTest.java
+++ src/test/java/cucumbertest/CucumberRunnerTest.java
@@ -5,7 +5,8 @@ import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
 
-import static io.cucumber.junit.platform.engine.Constants.*;
+import static io.cucumber.junit.platform.engine.Constants.FEATURES_PROPERTY_NAME;
+import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
 
 @Suite
 @IncludeEngines("cucumber")
